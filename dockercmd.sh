2144dc709df5a334af71ca4824ac94f7d4dcd341
alex-pl <alex@neatnet.de>
2016-11-29 17:33:14 +0100

    Allow spaces in the path of the working directory

--- dockercmd.sh
+++ dockercmd.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v $PWD:/data blang/latex "$@"
+exec docker run --rm -i --user="$(id -u):$(id -g)" --net=none -v "$PWD":/data blang/latex "$@"
